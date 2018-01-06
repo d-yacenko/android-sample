258dcce540f01c1eb92e32e1a1cabf2a33996cb6
D.Yacenko <d.yacenko@gmail.com>
2018-01-06 10:48:56 +0300

    InstrumentalUITestSample

--- InstrumentalUITestSample/app/src/main/java/ru/samsung/itschool/book/myapplication/MainActivity.java
+++ InstrumentalUITestSample/app/src/main/java/ru/samsung/itschool/book/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+// About UI Testing see here - https://developer.android.com/training/testing/ui-testing/espresso-testing.html
+
 public class MainActivity extends Activity {
     ImageView image;
     EditText editText;
